0bf8780fb1390fc3d52dacbe05f612defa33300d
Mindoim 🥜 <galicianib@gmail.com>
2025-10-10 12:16:31 -0300

    Componente Menu - Versão mobile

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,16 @@
+import styles from './Menu.module.css'
+import LogoCapivaraMenu from '../../assets/capivara-menu.png'
+
+const Menu = () => {
+    return (
+        <div>
+            <ul className={styles.containerMenu}>
+                <li>Sobre nós</li>
+                <li><img className={styles.imagemCapivara} src={LogoCapivaraMenu} alt="Imagem de uma capivara no menu de navegação"/></li>
+                <li>Visite a loja</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
