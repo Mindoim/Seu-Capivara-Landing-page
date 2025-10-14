286ba6c9feda29dcda6c486f375c0252cb23947a
Mindoim 🥜 <galicianib@gmail.com>
2025-10-14 14:45:58 -0300

    Hero pronto

---
+++ src/components/Hero/ContainerPrincipalHero/AnimacaoDigitando/AnimacaoDigitando.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AnimacaoDigitando() {
+    return (
+        <div></div>
+    );
+}
+
+export default AnimacaoDigitando;
---
+++ src/components/Hero/ContainerPrincipalHero/Texts/Text.jsx
@@ -0,0 +1,13 @@
+import styles from './Text.module.css'
+
+const Text = () => {
+    return (
+        <div className={styles.containerSeuCapivara}>
+            <h1 className='heroHeadingMobile'>SEU
+                <span><br></br>CAPIVARA</span>
+            </h1>
+        </div>
+    )
+}
+
+export default Text
