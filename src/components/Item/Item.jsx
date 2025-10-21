9686d5dbf62eda9613a3a6de19a84efeb307f756
Mindoim 🥜 <galicianib@gmail.com>
2025-10-21 08:36:45 -0300

    arrumando

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,10 +1,8 @@
 import styles from './Item.module.css'
- import Carrosel from "./components/Carrosel/Carrosel.jsx";
 
 const Item = () => {
     return (
         <div className={styles.divItem}>
-            <Carrosel></Carrosel>
         </div>
     )
 }
